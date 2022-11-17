a40e5bac0d53a70cd6e63961d350b74c4cadb46f
superq <13815865+meooxx@users.noreply.github.com>
2022-11-17 14:51:49 +0800

    leetcode: 153

---
+++ findMinimumInRotatedSortedArray/main.go
@@ -0,0 +1,19 @@
+package main
+
+// 4  5  1 2 3
+// 4  5  1
+// 5  1
+// 1
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+
+	for left < right {
+		midIndex := left + (right-left)/2
+		if nums[midIndex] > nums[right] {
+			left = midIndex + 1
+		} else {
+			right = midIndex
+		}
+	}
+	return nums[left]
+}
