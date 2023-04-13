471355e62bb937d84b119b41a2c4cac5ba106dea
superq <13815865+meooxx@users.noreply.github.com>
2023-04-13 13:26:45 +0800

    leetcode: max sliding window

--- slidingWindowMaximum/main.go
+++ slidingWindowMaximum/main.go
@@ -1,6 +1,16 @@
 package main
 
-func maxSlidingWindow(nums []int, k int) []int {
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(maxSlidingWindow([]int{
+		1, 3, -1, -3, 5, 3, 6, 7,
+	}, 3))
+}
+
+func maxSlidingWindow1(nums []int, k int) []int {
 	maxHeap := &MaxHeap{&Node{}}
 	left, right := 0, 0
 	result := []int{}
@@ -13,6 +23,35 @@ func maxSlidingWindow(nums []int, k int) []int {
 		result = append(result, maxHeap.head.Next.Val)
 		maxHeap.pop(nums[left])
 		left++
+	}
+	return result
+}
+/*
+*   2 1 3 5 -1 2
+*  push(2)  [2]
+		 push(1) [2,1]
+		3>1 pop(), 3>2 pop() -> [] ->  push(3)
+		5>3	pop() push(5) -> [5]
+		-1<5 push(-1) -> [5, -1]
+		2 >-1<5, pop(), push(2) -> [5,2]
+*/
+func maxSlidingWindow(nums []int, k int) []int {
+	q := []int{} // indices
+	result := []int{}
+	for i := range nums {
+		for len(q) > 0 && nums[q[len(q)-1]] < nums[i] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+		// i - k == q[0] is same
+		// 1 4 2 5
+		//     if i-k == q[0], the i will be at 5 position
+    if i - k + 1 > q[0] {
+      q = q[1:]
+    }
+		if i-k+1 >= 0 {
+			result = append(result, nums[q[0]])
+		}
 	}
 	return result
 }
