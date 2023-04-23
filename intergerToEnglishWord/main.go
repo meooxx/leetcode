c729bb2abfbc88981ab7ab5842d627f7f16f3df2
superq <13815865+meooxx@users.noreply.github.com>
2023-04-23 15:55:22 +0800

    leetcode: interger to english word

---
+++ intergerToEnglishWord/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "strings"
+
+func numberToWords1(num int) string {
+	if num == 0 {
+		return "Zero"
+	}
+	digit1 := []string{
+		"", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+		"Eight", "Nine",
+	}
+	digit10 := []string{
+		"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
+		"Nineteen",
+	}
+	digit20 := []string{
+		"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety",
+	}
+	digit1000 := []string{
+		"Hundred", "Thousand", "Million", "Billion",
+	}
+	encode := func(num int) string {
+		result := []string{}
+		if num >= 100 {
+			index := num / 100
+			result = append(result, digit1[index])
+			result = append(result, digit1000[0])
+			// X Hundred
+			num %= 100
+		}
+		if num >= 10 {
+			if num < 20 {
+				result = append(result, digit10[num%10])
+			} else {
+				result = append(result, digit20[num/10])
+				result = append(result, digit1[num%10])
+				// 23
+				// twenty three
+			}
+		} else {
+			result = append(result, digit1[num])
+		}
+		return strings.TrimSpace(strings.Join(result, " "))
+	}
+	thousandIndex := 0
+	result := ""
+	for num > 0 {
+		d := num % 1000
+		encodeDecimal := encode(d)
+		// 加 thousand, million...
+		if encodeDecimal != "" && thousandIndex > 0 {
+			result = " " + digit1000[thousandIndex] + " " + result
+		}
+		result = encodeDecimal + result
+
+		num /= 1000
+		thousandIndex++
+	}
+	return strings.TrimSpace(result)
+}
