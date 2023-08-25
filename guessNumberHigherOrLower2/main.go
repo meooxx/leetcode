869d055259a337abc9c6a2c42b392f523bb7f64a
superq <13815865+meooxx@users.noreply.github.com>
2023-08-25 14:01:28 +0800

    perf: leetcode guess number higher or lower 2

--- guessNumberHigherOrLower2/main.go
+++ guessNumberHigherOrLower2/main.go
@@ -1,7 +1,8 @@
 package main
+
 import "math"
 
-func getMoneyAmount(n int) int {
+func getMoneyAmount0(n int) int {
 	dp := make([][]int, n+1)
 	for i := range dp {
 		dp[i] = make([]int, n+1)
@@ -38,4 +39,38 @@ func getMoney(left, right int, dp [][]int) int {
 	}
 	dp[left][right] = maximum
 	return maximum
+}
+
+
+
+//  1  2  3  4   5   
+//  1-2  -> 1
+//  2-3  -> 2
+//  3-4  -> 3
+//  4-5  -> 4
+// 
+// dp[i][i] == 0, e.g. 2-2, 1-1, 3-3 的范围,only 2
+// 1-5 =>
+// min
+//		1+Max(dp[1][0], d[2][5])
+//    2+Max(dp[1][1], d[3][5])     ✅
+//  	3 + Max(dp[1][2], dp[4][5] )
+// 
+
+func getMoneyAmount(n int) int {
+	dp := make([][]int, n+2)
+	for i := range dp {
+		dp[i] = make([]int, n+2)
+	}
+
+	for l := 1; l < n; l++ {
+		for i := 1; i <= n-l; i++ {
+			j := i + l
+			dp[i][j] = math.MaxInt
+			for k := i; k <= j; k++ {
+				dp[i][j] = min(dp[i][j], max(dp[i][k-1], dp[k+1][j])+k)
+			}
+		}
+	}
+	return dp[1][n]
 }
