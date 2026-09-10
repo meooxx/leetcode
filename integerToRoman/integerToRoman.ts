360f04d5cf9d26c4f7a0db0a2303b7d302eb63d4
superq <13815865+meooxx@users.noreply.github.com>
2026-09-10 01:10:07 -0400

    enhance intToRoman function

--- integerToRoman/integerToRoman.ts
+++ integerToRoman/integerToRoman.ts
@@ -43,4 +43,48 @@ function intToRoman1(num: number): string {
 
 };
 
+function intToRoman(num: number): string {
+	const map = new Map([
+		[3000, 'MMM'],
+		[2000, 'MM'],
+		[1000, 'M'],
+		[900, 'CM'],
+		[800, 'DCCC'],
+		[700, 'DCC'],
+		[600, 'DC'],
+		[500, "D"],
+		[400, 'CD'],
+		[300, 'CCC'],
+		[200, 'CC'],
+		[100, 'C'],
+		[90, 'XC'],
+		[80, 'LXXX'],
+		[70, 'LXX'],
+		[60, 'LX'],
+		[50, 'L'],
+		[40, "XL"],
+		[30, 'XXX'],
+		[20, 'XX'],
+		[10, "X"],
+		[9, 'IX'],
+		[8, 'VIII'],
+		[7, 'VII'],
+		[6, 'VI'],
+		[5, 'V'],
+		[4, 'IV'],
+		[3, 'III'],
+		[2, 'II'],
+		[1, 'I']])
+	let base = 1
+	let answer = ''
+	// 74
+	while (num > 0) {
+		const digit = num % 10
+		answer = (map.get(digit * base) ?? '') + answer
+		num = ~~(num / 10)
+		base *= 10
+	}
+	return answer
+};
+
 console.log(intToRoman(3749))
