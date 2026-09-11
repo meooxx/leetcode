4b78a6660528f128adad3df9ced65b75267983c9
superq <13815865+meooxx@users.noreply.github.com>
2026-09-11 10:03:44 -0400

    implement fourSum function

---
+++ 4sum/foursum.ts
@@ -0,0 +1,35 @@
+function fourSum(nums: number[], target: number): number[][] {
+	nums.sort((a, b) => a - b)
+	const answer: number[][] = []
+	for (let i = 0; i < nums.length - 3; i++) {
+		if (i > 0 && nums[i] === nums[i - 1]) {
+			continue
+		}
+		for (let j = i + 1; j < nums.length - 2; j++) {
+			if (j > i + 1 && nums[j] === nums[j - 1]) {
+				continue
+			}
+			let k = j+1;
+			let l = nums.length - 1
+			while (k < l) {
+				const sum = nums[i] + nums[j] + nums[k] + nums[l]
+				if (sum === target) {
+					answer.push([nums[i], nums[j], nums[k], nums[l]])
+					k++
+					l--
+					while (k < l && nums[k] === nums[k - 1]) {
+						k++
+					}
+					while (l > k && nums[l] === nums[l + 1]) {
+						l--
+					}
+				} else if (sum > target) {
+					l--
+				} else {
+					k++
+				}
+			}
+		}
+	}
+	return answer
+};
