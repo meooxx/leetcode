fb526b53ab653a5bc5d5b4a6f11260102fc5c777
superq <13815865+meooxx@users.noreply.github.com>
2026-09-11 09:29:18 -0400

    refactor letterCombinations function for improved  performance

--- letterCombination/letterCombination.ts
+++ letterCombination/letterCombination.ts
@@ -1,4 +1,4 @@
-function letterCombinations(digits: string): string[] {
+function letterCombinations1(digits: string): string[] {
 	const map = new Map<string, string>(
 		[
 			['2', 'abc'],
@@ -35,4 +35,28 @@ function generateCombination(candidates: string[]): string[] {
 		}
 	}
 	return result
+}
+
+
+function letterCombinations(digits: string): string[] {
+	const map = new Map([
+		["2", ["a", "b", "c"]],
+		["3", ["d", "e", "f"]],
+		["4", ["g", "h", "i"]],
+		["5", ["j", "k", "l"]],
+		["6", ["m", "n", "o"]],
+		["7", ["p", "q", "r", "s"]],
+		["8", ["t", "u", "v"]],
+		["9", ["w", "x", "y", "z"]]
+	])
+	const candidates = digits.split('')
+	return candidates.reduce<string[]>((pre: string[], curr: string) => {
+		const currArr = map.get(curr)!
+		return pre.flatMap((v: string) => {
+			return currArr.map((nextV: string) => {
+				return v + nextV
+			})
+		}
+		)
+	}, [''])
 }
