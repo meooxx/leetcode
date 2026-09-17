5917713b3a27fefc6fd1a3ca32a2154256b0fe61
superq <13815865+meooxx@users.noreply.github.com>
2026-09-16 22:42:47 -0400

    searchInRotatedArray: a more readable implement

--- searchInRotatedSortedArray/searchInRotatedSortedArray.ts
+++ searchInRotatedSortedArray/searchInRotatedSortedArray.ts
@@ -1,4 +1,4 @@
-function search(nums: number[], target: number): number {
+function search1(nums: number[], target: number): number {
 	let left = 0
 	let right = nums.length - 1
 	let midNum
@@ -21,6 +21,41 @@ function search(nums: number[], target: number): number {
 			right = mid - 1
 		} else {
 			left = mid + 1
+		}
+
+	}
+	return -1
+};
+
+
+function search(nums: number[], target: number): number {
+	let left = 0
+	let right = nums.length - 1
+  // two sorted sequence
+	// 4 5 6 7 1 2 3
+	// case1:  left ele is less than or equal to mid, 
+	// and target is in nums[left] ~ nums[mid]
+	//  4 5  1 2 3
+	// case2: left is great than mid
+	// and target is in nums[mid] ~ nums[right] 
+	while (left <= right) {
+
+		let mid = ~~((right - left) / 2) + left
+
+		if (nums[mid] === target) {
+			return mid
+		} else if (nums[left] <= nums[mid]) {
+			if (target >= nums[left] && target < nums[mid]) {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if (target > nums[mid] && target <= nums[right]) {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
 		}
 
 	}
