ec3e81fdca19109b32f33c1702c9cd57d1891428
superq <13815865+meooxx@users.noreply.github.com>
2026-09-18 14:01:48 -0400

    implement isValidSudoku function

---
+++ validSudoku/validSudoku.ts
@@ -0,0 +1,37 @@
+
+
+function isValidSudoku(board: string[][]): boolean {
+	const rowSeen = new Set<string>()
+	const colSeen = new Set<string>()
+	const blockSeen = new Set<string>()
+	for (let r = 0; r < 9; r++) {
+		for (let c = 0; c < 9; c++) {
+			if (board[r][c] === '.') continue
+			const rowChar = `${board[r][c]}-${r}-row`
+			const colChar = `${board[r][c]}-${c}-col`
+			const blockChar = `${board[r][c]}-${~~(r / 3)}/${~~(c / 3)}-block`
+			if (rowSeen.has(rowChar) || colSeen.has(colChar) || blockSeen.has(blockChar)) {
+				return false
+			}
+			rowSeen.add(rowChar)
+			colSeen.add(colChar)
+			blockSeen.add(blockChar)
+
+		}
+
+	}
+
+
+	return true
+}
+
+isValidSudoku([
+	["5", "3", ".", ".", "7", ".", ".", ".", "."],
+	["6", ".", ".", "1", "9", "5", ".", ".", "."],
+	[".", "9", "8", ".", ".", ".", ".", "6", "."],
+	["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+	["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+	["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+	, [".", "6", ".", ".", ".", ".", "2", "8", "."]
+	, [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+	, [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
